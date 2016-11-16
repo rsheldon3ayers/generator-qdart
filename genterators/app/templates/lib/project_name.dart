790da7221b120d2fb078724b5c7835bd009a63cd
Robert (Bobby) Ayers <rsheldon3ayers@gmail.com>
2016-11-16 11:23:12 -0600

    Create project_name.dart

---
+++ genterators/app/templates/lib/project_name.dart
@@ -0,0 +1,10 @@
+import 'package:stagexl/stagexl.dart';
+import 'package:quavertools/quavertools.dart';
+
+class ProjectName extends Sprite
+{
+  ProjectName()
+  {
+    print("Hello world!");
+  }
+}
